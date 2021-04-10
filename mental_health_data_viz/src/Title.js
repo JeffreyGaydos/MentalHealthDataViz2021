0dcf3a936ed60ee817e287778e985aa88b7a2a3f
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-04-10 10:26:14 -0400

    Frontend - Added Title

---
+++ mental_health_data_viz/src/Title.js
@@ -0,0 +1,12 @@
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Mental Health Data Visualization Competition</h1>
+      <p>4-10-2021</p>
+    </div>
+  );
+}
+
+export default Title;
--- mental_health_data_viz/src/index.js
+++ mental_health_data_viz/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Title from './Title';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Title />
   </React.StrictMode>,
   document.getElementById('root')
 );
